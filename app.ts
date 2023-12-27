838412cbee7cb087934aecf1cad39d0b7537a571
mritunjaysaha <mritunjaysaha17@gmail.com>
2023-12-28 01:11:25 +0530

    chore: migration to ts

--- app.ts
+++ app.ts
@@ -1,17 +1,18 @@
 require("dotenv").config();
 
-const express = require("express");
-const cors = require("cors");
-const cookieParser = require("cookie-parser");
+import express from "express";
+
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
 // ROUTES
-const wordRoutesV1 = require("./routes/v1/words.routes");
+import wordRoutesV1 from "./routes/v1/words.routes";
 
 const app = express();
 
 app.use(cors({ origin: true, credentials: true }));
 app.use(cookieParser());
-app.use(express.json({ extend: false }));
+app.use(express.json());
 
 app.get("/", (req, res) => {
     res.send("server up and running");
@@ -19,4 +20,4 @@ app.get("/", (req, res) => {
 
 app.use("/api/v1/words", wordRoutesV1);
 
-module.exports = { app };
+export { app };
