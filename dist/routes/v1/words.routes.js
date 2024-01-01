8f3a5fb79d0b5c02b96b4c1bcbd5e47b1fec4171
Mritunjay Saha <mritunjaysaha17@gmail.com>
2024-01-01 23:41:11 +0530

    Revert "feat: add word in user collection"

--- dist/routes/v1/words.routes.js
+++ dist/routes/v1/words.routes.js
@@ -1,9 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
-const auth_controller_1 = require("../../controllers/auth.controller");
-const user_controller_1 = require("../../controllers/user.controller");
 const word_controller_1 = require("../../controllers/word.controller");
+const user_controller_1 = require("../../controllers/user.controller");
 const router = (0, express_1.Router)();
 router.param("userId", user_controller_1.getUserById);
 /**
@@ -16,12 +15,6 @@ router.get("/", word_controller_1.getWord);
  * @route /api/v1/auth/words/:uerId
  * @params userId
  */
-router.get("/:userId", auth_controller_1.isSignedIn, auth_controller_1.isAuthenticated, word_controller_1.getWordForUser);
-/**
- * @method GET
- * @route /api/v1/auth/words/:uerId
- * @params userId
- */
-router.put("/:userId", auth_controller_1.isSignedIn, auth_controller_1.isAuthenticated, word_controller_1.putWordInUser);
+router.get("/:userId", word_controller_1.getWord);
 exports.default = router;
 //# sourceMappingURL=words.routes.js.map
