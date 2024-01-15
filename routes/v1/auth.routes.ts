6e8d604b642d78a3103cf6f057d5c259ed67dc38
mritunjaysaha <mritunjaysaha17@gmail.com>
2024-01-15 23:57:06 +0530

    feat: added google oauth callback route

--- routes/v1/auth.routes.ts
+++ routes/v1/auth.routes.ts
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { Router } from "express";
 import { check } from "express-validator";
 import {
@@ -57,9 +58,46 @@ router.get("/is-signed-in", isSignedIn, (req, res) => {
 });
 
 router.get("/google", (req, res) => {
-    const url = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${process.env.GOOGLE_OAUTH_CLIENT_ID}&redirect_uri=${process.env.REDIRECT_URI}&response_type=code&scope=profile email`;
+    const url = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${process.env.GOOGLE_OAUTH_CLIENT_ID}&redirect_uri=${process.env.GOOGLE_OAUTH_REDIRECT_URL}&response_type=code&scope=profile email`;
 
     res.redirect(url);
+});
+
+router.get("/google/callback", async (req, res) => {
+    const { code } = req.query;
+
+    console.log({ code });
+
+    try {
+        const { data } = await axios.post(
+            "<https://oauth2.googleapis.com/token>",
+            {
+                client_id: process.env.GOOGLE_OAUTH_CLIENT_ID,
+                client_secret: process.env.GOOGLE_OAUTH_CLIENT_SECRET,
+                code,
+                redirect_uri: process.env.GOOGLE_OAUTH_REDIRECT_URL,
+                grant_type: "authorization_code",
+            }
+        );
+
+        const { access_token, id_token } = data;
+
+        console.log({ access_token, id_token });
+
+        const { data: profile } = await axios.get(
+            "<https://www.googleapis.com/oauth2/v1/userinfo>",
+            {
+                headers: { Authorization: `Bearer ${access_token}` },
+            }
+        );
+
+        console.log({ profile });
+
+        res.redirect("/");
+    } catch (error) {
+        console.error("Error:", error.response.data.error);
+        res.redirect("/login");
+    }
 });
 
 export default router;
