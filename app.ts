91950c4698a1a181b49f3eaa5d6e19da05502b3b
mritunjaysaha <mritunjaysaha17@gmail.com>
2024-01-23 23:13:28 +0530

    feat: added google auth

--- app.ts
+++ app.ts
@@ -4,10 +4,15 @@ import express from "express";
 
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import session from "express-session";
+import passport from "passport";
+
+import googleStrategy from "passport-google-oauth20";
 
 import { connectDB } from "./config/db.config";
 
 // ROUTES
+import { UserModel } from "./models/user.model";
 import authRoutesV1 from "./routes/v1/auth.routes";
 import userRoutesV1 from "./routes/v1/user.routes";
 import wordRoutesV1 from "./routes/v1/words.routes";
@@ -22,6 +27,86 @@ connectDB();
 app.use(cors({ origin: true }));
 app.use(cookieParser());
 app.use(express.json());
+app.use(
+    session({
+        secret: process.env.SECRET, //secret used to sign the session ID cookie
+        resave: true, //save session on every request
+        saveUninitialized: true, //save uninitialized sessions (new and not modified)
+        cookie: {
+            sameSite: "none", //allow cross-site requests from different origin
+            secure: true, //requires HTTPS. For local environment you may skip this.
+            maxAge: 1000 * 60 * 60 * 24 * 7, // One Week
+        },
+    })
+);
+
+app.use(passport.initialize()); //initialize Passport authentication
+app.use(passport.session()); //use Passport for session authentication
+
+passport.serializeUser((user, done) => {
+    done(null, user._id);
+});
+
+passport.deserializeUser((id, done) => {
+    UserModel.findById(id, (err, doc) => {
+        done(null, doc);
+    });
+});
+
+//Google Strateg
+passport.use(
+    new googleStrategy(
+        {
+            clientID: process.env.GOOGLE_OAUTH_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_OAUTH_CLIENT_SECRET,
+            callbackURL: process.env.GOOGLE_OAUTH_CALLBACK_URL,
+        },
+
+        function (accessToken, refreshToken, profile, cb) {
+            UserModel.findOne({ googleId: profile.id }, async (err, user) => {
+                if (err) return cb(err, null);
+                if (!user) {
+                    const newUser = new UserModel({
+                        googleId: profile.id,
+                        username: profile.displayName,
+                    });
+                    await newUser.save();
+                    cb(null, newUser);
+                }
+                cb(null, user);
+            });
+        }
+    )
+);
+
+app.get(
+    "/auth/google",
+    passport.authenticate("google", { scope: ["profile"] })
+);
+
+app.get(
+    "/auth/google/callback",
+    passport.authenticate("google", { failureRedirect: "/login" }),
+    (req, res) => {
+        //successful authentication, redirect home
+        console.log({ req });
+        res.redirect(process.env.CLIENT_URL);
+    }
+);
+
+app.get("/getuser", (req, res) => {
+    res.send(req.user);
+});
+
+app.get("/auth/logout", function (req, res, next) {
+    req.logout(function (err) {
+        console.log("logged out");
+        if (err) {
+            return next(err);
+        }
+        res.send("done");
+    });
+});
 
 app.get("/", (req, res) => {
     res.send("server up and running");
--- dist/app.js
+++ dist/app.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -8,8 +17,12 @@ require("dotenv").config();
 const express_1 = __importDefault(require("express"));
 const cookie_parser_1 = __importDefault(require("cookie-parser"));
 const cors_1 = __importDefault(require("cors"));
+const express_session_1 = __importDefault(require("express-session"));
+const passport_1 = __importDefault(require("passport"));
+const passport_google_oauth20_1 = __importDefault(require("passport-google-oauth20"));
 const db_config_1 = require("./config/db.config");
 // ROUTES
+const user_model_1 = require("./models/user.model");
 const auth_routes_1 = __importDefault(require("./routes/v1/auth.routes"));
 const user_routes_1 = __importDefault(require("./routes/v1/user.routes"));
 const words_routes_1 = __importDefault(require("./routes/v1/words.routes"));
@@ -22,6 +35,63 @@ exports.app = app;
 app.use((0, cors_1.default)({ origin: true }));
 app.use((0, cookie_parser_1.default)());
 app.use(express_1.default.json());
+app.use((0, express_session_1.default)({
+    secret: process.env.SECRET, //secret used to sign the session ID cookie
+    resave: true, //save session on every request
+    saveUninitialized: true, //save uninitialized sessions (new and not modified)
+    cookie: {
+        sameSite: "none", //allow cross-site requests from different origin
+        secure: true, //requires HTTPS. For local environment you may skip this.
+        maxAge: 1000 * 60 * 60 * 24 * 7, // One Week
+    },
+}));
+app.use(passport_1.default.initialize()); //initialize Passport authentication
+app.use(passport_1.default.session()); //use Passport for session authentication
+passport_1.default.serializeUser((user, done) => {
+    done(null, user._id);
+});
+passport_1.default.deserializeUser((id, done) => {
+    user_model_1.UserModel.findById(id, (err, doc) => {
+        done(null, doc);
+    });
+});
+//Google Strateg
+passport_1.default.use(new passport_google_oauth20_1.default({
+    clientID: process.env.GOOGLE_OAUTH_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_OAUTH_CLIENT_SECRET,
+    callbackURL: process.env.GOOGLE_OAUTH_CALLBACK_URL,
+}, function (accessToken, refreshToken, profile, cb) {
+    user_model_1.UserModel.findOne({ googleId: profile.id }, (err, user) => __awaiter(this, void 0, void 0, function* () {
+        if (err)
+            return cb(err, null);
+        if (!user) {
+            const newUser = new User({
+                googleId: profile.id,
+                username: profile.displayName,
+            });
+            yield newUser.save();
+            cb(null, newUser);
+        }
+        cb(null, user);
+    }));
+}));
+app.get("/auth/google", passport_1.default.authenticate("google", { scope: ["profile"] }));
+app.get("/auth/google/callback", passport_1.default.authenticate("google", { failureRedirect: "/login" }), (req, res) => {
+    //successful authentication, redirect home
+    res.redirect(process.env.CLIENT_URL);
+});
+app.get("/getuser", (req, res) => {
+    res.send(req.user);
+});
+app.get("/auth/logout", function (req, res, next) {
+    req.logout(function (err) {
+        console.log("logged out");
+        if (err) {
+            return next(err);
+        }
+        res.send("done");
+    });
+});
 app.get("/", (req, res) => {
     res.send("server up and running");
 });
